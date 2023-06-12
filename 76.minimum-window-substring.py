fc479f8187349ac0753c0932830fb122f8e7fde2
AbdoMgdy <abdoman46@hotmail.com>
2023-06-12 14:25:21 +0300

    76  Minimum Window Substring

--- 76.minimum-window-substring.py
+++ 76.minimum-window-substring.py
@@ -12,32 +12,36 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        if t=="": return ""
 
-        countT,window= {},{}
-        have,need=0,len(t)
-        res,resLen=[0,0],float('infinity')
-        l=0
+        if t == "":
+            return ""
+
+        countT, window = {}, {}
         for c in t:
-            countT[c]=1+countT.get(c,0)
-        
+            countT[c] = 1 + countT.get(c, 0)
+
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
         for r in range(len(s)):
             c = s[r]
-            window[c]=1+window.get(c,0)
+            window[c] = 1 + window.get(c, 0)
 
-            if c in countT and window[c] <= countT[c]: # why <= and not ==
-                have+=1
-            while have==need:
-                if (r-l+1)<resLen:
-                    res = [l,r]
-                    resLen=r-l+1
-                window[s[l]]-=1
-                if s[l] in countT and window[s[l]]<countT[s[l]]:
-                    have-=1
-                l+=1
-        l,r = res
-        return s[l:r+1] if resLen!=float("infinity") else ""
-        
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            while have == need:
+                # update our result
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                # pop from the left of our window
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l : r + 1] if resLen != float("infinity") else "" 
         
 # @lc code=end
 
