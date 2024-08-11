b617520df518a7549bca6d760ba59ad9368c63d9
AbdoMgdy <abdoman46@hotmail.com>
2024-08-11 17:40:29 +0300

    add more problems

---
+++ 347.top-k-frequent-elements.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=347 lang=python
+#
+# [347] Top K Frequent Elements
+#
+
+
+# @lc code=start
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        res = []
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+
+        for n, c in count.items():
+            freq[c].append(n)
+
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+
+# @lc code=end
---
+++ 49.group-anagrams.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=49 lang=python
+#
+# [49] Group Anagrams
+#
+
+# @lc code=start
+
+
+from collections import defaultdict
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        res = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            res(tuple(count)).append(s)
+
+        return res.values()
+
+
+# @lc code=end
